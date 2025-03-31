a109b2dd7b74e42497852dd938bc476318cebb62
Gaurish Todi <gaurishtodi@gmail.com>
2025-04-01 00:28:19 +0530

    at one moreplace

--- GoRide/src/components/Driver/NavBarDriver.js
+++ GoRide/src/components/Driver/NavBarDriver.js
@@ -24,7 +24,7 @@ const NavbarDriver = () => {
         localStorage.removeItem("email");
         localStorage.removeItem("phone");
         auth.signOut();
-        navigate('/home');
+        navigate('/');
     };
 
     return (
